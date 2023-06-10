79f0a3d9ba6f4114f36e392cf9b5fd8c1ba8757b
Anatoliy Basharov <rexonka3@gmail.com>
2023-06-10 22:43:40 +0300

    Complete HomeTask1

--- HomeTask1/Program.cs
+++ HomeTask1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число ");
+string NumberOne = Console.ReadLine();
+Console.WriteLine("Введите второе число ");
+string NumberTwo = Console.ReadLine();
+int One = Convert.ToInt32(NumberOne);
+int Two = Convert.ToInt32(NumberTwo);
+if(One > Two)
+{
+    Console.WriteLine($"max = {One}");
+    Console.WriteLine($"min = {Two}");
+} else
+{
+    if(One < Two)
+    {
+    Console.WriteLine($"max = {Two}");
+    Console.WriteLine($"min = {One}"); 
+    }
+}
+
+
+
+
