ddac4af3fc1caf26c4d798d6553ee9753b38446a
Anatoliy Basharov <rexonka3@gmail.com>
2023-06-11 22:06:53 +0300

    Finish HomeTask4 and finish HomeWork

---
+++ HomeTask4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+
+Console.WriteLine("Введите число ");
+string number = Console.ReadLine();
+int N = Convert.ToInt32(number);
+int temp = 0;
+while(temp < N)
+{ 
+    temp++;
+    if(temp % 2 ==0)
+{
+Console.Write($" {temp}");
+}
+    
+}
+
+
