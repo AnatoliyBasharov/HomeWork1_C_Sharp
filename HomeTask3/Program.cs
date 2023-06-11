e4b279ed84566d33102af8864373dd01a7b7d46e
Anatoliy Basharov <rexonka3@gmail.com>
2023-06-11 15:28:47 +0300

    Finish HomeTask3

--- HomeTask3/Program.cs
+++ HomeTask3/Program.cs
@@ -2,5 +2,18 @@
 
 
 
-Console.WriteLine();
+Console.WriteLine("Введите число ");
+string InputNumber = Console.ReadLine();
+int Number = Convert.ToInt32(InputNumber);
+int max = Number;
+int temp = Number;
+if(Number % 2 == 0)
+{
+Console.WriteLine($" Ого,это число {Number} и в правду является четным!");
+}
+if(temp % 2 != 0)
+{
+ Console.WriteLine($"О нет, это число {temp} не является четным :(");   
+}
+
 
